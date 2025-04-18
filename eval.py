548e549c05c5aeb730613e3c10ff4930300169c3
Alex Huo <alexhuo2020@gmail.com>
2025-04-18 14:10:59 -0500

    update README

--- eval.py
+++ eval.py
@@ -1,27 +1,42 @@
-from simpleLLM import SimpleLLM, SimpleLLMConfig, generate, TokenizerWithSystem
+from simpleLLM import SimpleLLM, SimpleLLMConfig, generate, Tokenizer, TokenizerWithSystem
 from post_train import resize_token_embeddings
 
 
 import torch
 import torch.nn as nn 
 import torch.nn.functional as F
+import argparse
 
-model = SimpleLLM(SimpleLLMConfig)
+parser = argparse.ArgumentParser(description="Evaluation script for different model types")
+parser.add_argument(
+    "--model_type",
+    type=str,
+    choices=["pretrained", "sft", "rlhf"],
+    required=True,
+    help="Specify the model type to evaluate: pretrained, sft, or rlhf"
+)
 
-model.embedding = resize_token_embeddings(model.embedding, 2)
-model.output_proj = nn.Linear(SimpleLLMConfig.embed_dim, SimpleLLMConfig.vocab_size + 2, bias=False)
-model.output_proj.weight = model.embedding.weight  # re-tie after replacing the embedding
-model.load_state_dict(torch.load("model_rlhf.pt", weights_only=True))
-tokenizer = TokenizerWithSystem()
+args = parser.parse_args()
 
-X = ["human do I like coffee . system",
+if args.model_type == "sft" or args.model_type == "rlhf":
+    SimpleLLMConfig.vocab_size += 2
+    model = SimpleLLM(SimpleLLMConfig)
+    model.load_state_dict(torch.load("model_%s.pt"%args.model_type, weights_only=True))
+    tokenizer = TokenizerWithSystem()
+    X = ["human do I like coffee . system",
      "human do You like coffee . system"]
+
+else:
+    model = SimpleLLM(SimpleLLMConfig)
+    model.load_state_dict(torch.load("model.pt", weights_only=True))
+    tokenizer = Tokenizer()
+    X = ["I like", "You like", "I do not like", "You do not like"]
+
+
 
 for x in X:
     x = torch.tensor([tokenizer.encode(x)])
-
     logits = model(x)
-    print(F.softmax(logits, -1))    
     for _ in range(10):
         y = generate(model, x, 2)
         for yy in y:
